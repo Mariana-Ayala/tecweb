9ded52ee51780ceaa81656827c3be171398f9332
Mariana <mariana.ayala2111@gmail.com>
2024-09-12 00:37:55 -0600

    Agregar p07 ejercicio 1

---
+++ practica_1/practicas/p07/src/funciones.php
@@ -0,0 +1,52 @@
+<?php
+
+// Función para comprobar si un número es múltiplo de 5 y 7
+function esMultiploDe5Y7($numero) {
+    return $numero % 5 == 0 && $numero % 7 == 0;
+}
+// impar, par, impar
+function generarSecuenciaImparParImpar() {
+    $matriz = [];
+    $iteraciones = 0;
+
+    do {
+        $iteraciones++;
+        $fila = [];
+        for ($i = 0; $i < 3; $i++) {
+            $fila[] = rand(1, 1000);
+        }
+        $matriz[] = $fila;
+    } while (!($matriz[count($matriz) - 1][0] % 2 != 0 &&
+               $matriz[count($matriz) - 1][1] % 2 == 0 &&
+               $matriz[count($matriz) - 1][2] % 2 != 0));
+
+    return [$matriz, $iteraciones];
+}
+//usando while
+function encontrarMultiploConWhile($numero) {
+    do {
+        $aleatorio = rand(1, 1000);
+    } while ($aleatorio % $numero != 0);
+
+    return $aleatorio;
+}
+//usando do-while
+function encontrarMultiploConDoWhile($numero) {
+    $aleatorio = rand(1, 1000);
+    while ($aleatorio % $numero != 0) {
+        $aleatorio = rand(1, 1000);
+    }
+
+    return $aleatorio;
+}
+// arreglo de caracteres ASCII
+function crearArregloAscii() {
+    $arreglo = [];
+    for ($i = 97; $i <= 122; $i++) {
+        $arreglo[$i] = chr($i);
+    }
+
+    return $arreglo;
+}
+?>
+
---
+++ practica_1/practicas/p07/src/index.php
@@ -0,0 +1,29 @@
+<?php
+// Incluye el archivo de funciones
+include 'C:\xampp\htdocs\tecweb\practica_1\practicas\p07\src\funciones.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 7</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+        if(isset($_GET['numero'])) {
+            $num = $_GET['numero'];
+            if (esMultiploDe5Y7($num)) {
+                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+            } else {
+                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+            }
+        }
+    ?>
+</body>
+</html>

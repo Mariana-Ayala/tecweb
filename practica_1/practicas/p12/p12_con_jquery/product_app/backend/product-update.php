29f86a4908bf7eb91ad75c40544a22c5222dec6b
Mariana <mariana.ayala2111@gmail.com>
2024-11-05 21:20:03 -0600

    p12

--- practica_1/practicas/p12/p12_con_jquery/product_app/backend/product-update.php
+++ practica_1/practicas/p12/p12_con_jquery/product_app/backend/product-update.php
@@ -9,21 +9,7 @@ $response = array('status' => 'error', 'message' => 'Error al actualizar el prod
 if (isset($data['id']) && isset($data['producto'])) {
     $id = $data['id'];
     $producto = $data['producto'];
-
-    // Validaciones
-    if (strlen($producto['detalles']) > 250) {
-        $response['message'] = 'Los detalles deben tener 250 caracteres o menos.';
-        echo json_encode($response, JSON_PRETTY_PRINT);
-        exit;
-    }
-
-    if ($producto['precio'] < 99) {
-        $response['message'] = 'El precio debe ser mayor o igual a 99.';
-        echo json_encode($response, JSON_PRETTY_PRINT);
-        exit;
-    }
     
-    // Preparar la consulta
     $sql = "UPDATE productos SET 
         nombre = '{$producto['nombre']}', 
         marca = '{$producto['marca']}', 
@@ -33,13 +19,12 @@ if (isset($data['id']) && isset($data['producto'])) {
         unidades = {$producto['unidades']}, 
         imagen = '{$producto['imagen']}' 
         WHERE id = {$id}";
-
-    // Ejecutar la consulta
+    
     if ($conexion->query($sql)) {
         $response['status'] = 'success';
         $response['message'] = 'Producto actualizado correctamente';
     } else {
-        $response['message'] = 'Error en la consulta: ' . mysqli_error($conexion);
+        $response['message'] = 'Error en la consulta: '.mysqli_error($conexion);
     }
 }
 

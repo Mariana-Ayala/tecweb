a821118e17ccc4aac546e67824f6d38f3aa4c06a
Mariana <mariana.ayala2111@gmail.com>
2024-11-30 12:19:43 -0600

    prueba con post

--- practica_1/practicas/p18/pruebaslim/index.php
+++ practica_1/practicas/p18/pruebaslim/index.php
@@ -19,6 +19,14 @@ $app->get("/hola[/{nombre}]", function($request, $response, $args){
     return $response;
 });
 
+$app->post("/pruebapost", function($request, $response, $args){
+    $reqPost = $request->getParsedBody();
+    $val1 = $reqPost["val1"];
+    $val2 = $reqPost["val2"];
+
+    $response->write("valores: ".$val1 ." ".$val2);
+    return $response;
+});
 
 
 $app->run();
